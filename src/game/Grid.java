225f4ff500f2987f3557cc399b87918cd4ee0a77
AntBen7793 <75934911+AntBen7793@users.noreply.github.com>
2021-05-06 16:42:23 +0200

    Update Grid.java

--- src/game/Grid.java
+++ src/game/Grid.java
@@ -21,7 +21,12 @@ public class Grid {
 	}
 	
 	public void randomInit() {
-
+		String[] letters = {"A", "B", "C", "D", "E", "F", "G", "H", "I", "J"};  
+		String[] num = {"1", "2", "3", "4", "5", "6", "7", "8", "9", "10"};  
+		java.util.Random random = new java.util.Random();
+		int random_y = random.nextInt(letters.length);
+		int random_x = random.nextInt(num.length);
+		System.out.println(letters[random_y] + num[random_x]);
 	}
 	
 	public int getValue(int column, int line) {
@@ -29,17 +34,68 @@ public class Grid {
 	}
 	
 	public void addShot(int column, int line, boolean success) {
+		if(success)
+		{
+			mat[line][column]=1;
+		}
+		else 
+		{
+			mat[line][column]=4;
+		}
 	}
 	
 	public boolean addNewShip(int column, int line, int s, int d) {
-		return true;
+		if(isValidShip(column, line, s, d))
+		{
+			placeShip(column, line, s, d);
+			return true;
+		}
+		else
+		{
+			return false;
+		}
 	}
 	
 	private void placeShip (Coordinates coord, int size, int dir) {
+		if(dir==0)
+		{
+			for(int i=l;i<size;i++)
+			{
+				mat[i][c]=1;
+			}
+		}
+		else
+		{
+			for(int i=c;i<size;i++)
+			{
+				mat[l][i]=1;
+			}
+		}
 	}
 	
 	private boolean isValidShip(Coordinates coord, int size, int dir) {
-        return true;
+        	if(dir==0)
+		{
+			if((GRID_SIZE-size)>=l)
+			{
+				return true;
+			}
+			else
+			{
+				return false;
+			}
+		}
+		else
+		{
+			if((GRID_SIZE-size)>=c)
+			{
+				return true;
+			}
+			else
+			{
+				return false;
+			}
+		}
 	}
 	
 	public String toString(){
