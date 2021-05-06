15e06751c0093313766b0b04954159aa4cd56e1a
MDKayzeur <74770566+MDKayzeur@users.noreply.github.com>
2021-05-06 16:29:40 +0200

    Update Grid.java

--- src/game/Grid.java
+++ src/game/Grid.java
@@ -1,110 +1,75 @@
 package game;
 
-import java.util.ArrayList;
-
-public class Grid {
-	private static final int GRID_SIZE = 10;
-	private int mat[][];
-
-	public Grid() {
-		mat = new int[GRID_SIZE][GRID_SIZE];
-		initMat();
-	}
-	private void initMat() {
-		for(int i=0;i<GRID_SIZE;i++){
-			for(int j=0;j<GRID_SIZE;j++){
-				mat[i][j]=0;
-			}
-		}
-	}
+public class Player {
+	private Grid playerGrid;
+	private Grid shotGrid;
+	private String name;
 	
-	public void randomInit() {
-		int l = (int) Math.random()*10;
-		int c = (int) Math.random()*10;
-		int dir = (int) Math.random()*2;
-		int s = 5;
-		int i;
-		for(i=0;i<7;i++) {
-			if(isValidShip(c,l,s,dir)==true) {
-				addNewShip(c,l,s,dir);
-				s--;
-				if(s==2){
-					addNewShip(c,l,s,dir);
-					i++;
-				}
-				if(s==1){
-					addNewShip(c,l,s,dir);
-					i++;
-				}
-			}
-		}
+	public Player(String n) {
+		this.playerGrid = new Grid();
+		this.shotGrid = new Grid();
+		this.name = n;
 	}
-	
-	public int getValue(int column, int line) {
-		return mat[column][line];
+	public boolean isValidShip(Coordinates coord, int size, int dir) {
+        return true;
 	}
-	
-	public void addShot(int column, int line, boolean success) {
-		if(success==true) {
-			mat[column][line] = 1;
-		}else {
-			mat[column][line] = 4;
-		}
-	}
-	
 	public boolean addNewShip(int column, int line, int s, int d) {
-		if(isValidShip(column,line,s,d)==true) {
-			placeShip(column, line, s,d);
+		Coordinates c = new Coordinates(column,line);
+		if((column>=0 && column<=9) && (line>=0 && line<=9) && (s<5 && s>0) && (d==1 && d==0)) {
 			return true;
-		}else {
+		}else{
 			return false;
 		}
 	}
-	private void placeShip (int c,int l, int size, int dir) {
-		if(dir==0)
+	public boolean recordShot(int column, int line, Player p) {
+		boolean touche=false;
+		if(shotGrid.getValue(column, line)==0){
+			if(p.hasShip(column, line)==true){
+				touche=true;
+			}
+			shotGrid.addShot(column, line, touche);
+		}
+		return touche;
+	}
+	private boolean hasShip(int c, int l) {
+		if(playerGrid.getValue(c, l)==1){
+			return true;
+		}else return false;
+	}
+	
+	public void initGridRandom() {
+		playerGrid.randomInit();
+	}
+	public void displayGrid() {
+		playerGrid.toString();
+	}
+	public void displayShotGrid() {
+		shotGrid.toString();
+	}
+	public boolean hasWin(){
+		int nb = 0;
+		for(int i=0;i<Grid.getGRIDSIZE();i++)
 		{
-			for(int i=l;i<l+size;i++){
-				if(mat[i][c]!=1) {
-					mat[i][c]=1;
-				}else {
-					System.out.println("Il y a déja un bâteau");
+			for(int j=0;j<Grid.getGRIDSIZE();j++)
+			{
+				if(shotGrid.getValue(i, j)==1)
+				{
+					nb++;
 				}
 			}
 		}
-		else{
-			for(int i=c;i<c+size;i++){
-				if(mat[l][i]!=1) {
-					mat[l][i]=1;
-				}else {
-					System.out.println("Il y a déja un bâteau");
-				}
-			}
+		if(nb==18)
+		{
+			return true;
+		}
+		else
+		{
+		return false;
 		}
 	}
 	
-	private boolean isValidShip(int c, int l, int size, int dir) {
-		if((c>=0 && c<=9) && (l>=0 && l<=9) && (size<6 && size>0) && (dir==1 && dir==0) && (mat[c][l]!=1)){
-			return true;
-		}else {
-			return false;
-		}
+	public String getName() {
+		return name;
 	}
-	public String toString(){
-		String str="      A  B  C  D  E  F  G  H  I  J\n    -------------------------------\n";
-		for(int i=0;i<GRID_SIZE;i++){
-			str += i +"  |";
-			for(int j=0;j<GRID_SIZE;j++)
-			{
-				str+="  "+mat[i][j];
-			}
-			str += " |\n";
-		}
-		 str += "    -------------------------------\n";
-		return str;
-	}
-	public static int getGRIDSIZE()
-	{
-		return GRID_SIZE;
-	}
+	
 }
-
