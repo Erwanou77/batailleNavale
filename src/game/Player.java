7c24bcb5d661b8b104e84582a99c8f24f1e45f79
AntBen7793 <75934911+AntBen7793@users.noreply.github.com>
2021-05-06 16:40:35 +0200

    Update Player.java

--- src/game/Player.java
+++ src/game/Player.java
@@ -13,18 +13,42 @@ public class Player {
 	}
 	
 	public boolean addNewShip(int column, int line, int s, int d) {
-		return false;
+		if((column>=0 && column<=9) &&(line>=0 && line<=9) && (s<=5 && s>0) &&( d==1 || d==0))
+		{
+			return playerGrid.addNewShip(column, line, s ,d);
+		}
+		else
+		{
+			return false;
+		}
 	}
 	
 	public boolean recordShot(int column, int line, Player p) {
-		return false;
+		boolean touche=false;
+		if(shotGrid.getValue(column, line)==0)
+		{
+			if(p.hasShip(column, line))
+			{
+				touche=true;
+			}
+			shotGrid.addShot(column, line, touche);
+		}
+		return touche;
 	}
 	
 	private boolean hasShip(int c, int l) {
-		return false;
+		if(playerGrid.getValue(c, l)==1)
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		}
 	}
 	
 	public void initGridRandom() {
+		playerGrid.randomInit();
 	}
 	
 	public void displayGrid() {
@@ -35,7 +59,25 @@ public class Player {
 	}
 	
 	public boolean hasWin(){
+		int nb = 0;
+		for(int i=0;i<Grid.getGRIDSIZE();i++)
+		{
+			for(int j=0;j<Grid.getGRIDSIZE();j++)
+			{
+				if(shotGrid.getValue(i, j)==1)
+				{
+					nb++;
+				}
+			}
+		}
+		if(nb==18)
+		{
+			return true;
+		}
+		else
+		{
 		return false;
+		}
 	}
 	
 	public String getName() {
